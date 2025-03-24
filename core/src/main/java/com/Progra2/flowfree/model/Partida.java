64758acfc4f6c45d6b0d0d7cc56568f635f4c563
NadFuentes <nadfuentes03@unitec.edu>
2025-03-23 21:27:53 -0600

    Again

--- core/src/main/java/com/Progra2/flowfree/model/Partida.java
+++ core/src/main/java/com/Progra2/flowfree/model/Partida.java
@@ -1,5 +1,59 @@
 package com.Progra2.flowfree.model;
 
-public class Partida {
+import java.io.Serializable;
+import java.util.Date;
 
+/**
+ * 
+ * 
+ * @author Nadiesda Fuentes
+ */
+public class Partida implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private int nivel;
+    private long tiempo;
+    private boolean completado;
+    private Date fecha;
+    
+    public Partida(int nivel, long tiempo, boolean completado) {
+        this.nivel = nivel;
+        this.tiempo = tiempo;
+        this.completado = completado;
+        this.fecha = new Date(); // Fecha y hora actual
+    }
+    
+    public Partida(int nivel, long tiempo, boolean completado, Date fecha) {
+        this.nivel = nivel;
+        this.tiempo = tiempo;
+        this.completado = completado;
+        this.fecha = fecha;
+    }
+    
+
+    public int getNivel() {
+        return nivel;
+    }
+    
+
+    public long getTiempo() {
+        return tiempo;
+    }
+    
+    public boolean isCompletado() {
+        return completado;
+    }
+    
+    public Date getFecha() {
+        return fecha;
+    }
+    
+    @Override
+    public String toString() {
+        return "Nivel: " + nivel + 
+               ", Fecha: " + fecha + 
+               ", Tiempo: " + tiempo + "s" + 
+               ", Completado: " + (completado ? "Sí" : "No");
+    }
 }
